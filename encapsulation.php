3e5555a71b70970adde5fa3b85732af63d73440a
page0715 <page0715@gmail.com>
2023-11-25 01:13:28 +0800

    增加encapsulation

---
+++ encapsulation.php
@@ -0,0 +1,28 @@
+<h1>封裝(Encapsulation)</h1>
+<?
+class Animal{
+    public $name;
+
+    public function setName($name){
+    $this->name=$name;
+}
+
+    public function getName(){
+        return $this->name;
+    }
+
+}
+
+$animal=new Animal; //實例化 instant
+
+echo '顯示名稱:'.$animal->name;
+echo '顯示名稱:'.$animal->getName();
+echo "<br>";
+$animal->setName('小花');
+echo '顯示名稱:'.$animal->getName();
+echo "<br>";
+$animal->name='阿中';
+echo '顯示名稱:'.$animal->name;
+echo "<br>";
+
+?>
