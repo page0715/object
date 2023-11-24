a8f87fd069e79f486e694569e35c3bcf975b95f8
page0715 <page0715@gmail.com>
2023-11-24 13:36:00 +0800

    animal

---
+++ animal.php
@@ -0,0 +1,35 @@
+<?php
+
+class Animal{
+
+
+}
+
+interface bark{
+
+    function b();
+
+
+}
+
+
+class Dog extends Animal implements bark{
+    function b(){
+        return "汪";
+    }
+}
+
+class Cat extends Animal{
+    function b(){
+        return "喵";
+    }
+}
+
+$dog=new Dog;
+$cat=new Cat;
+
+echo $dog->b();
+echo "<br>";
+echo $cat->b();
+
+
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+// class Animal
+// {
+//     // public $name;
+//     protected $name;
+
+//     public function __construct($name)
+//     {
+//         $this->name = $name;
+//     }
+
+//     public function setName($name)
+//     {
+//         //$this 指 Animal 這個物件
+//         $this->name = $name;
+//     }
+//     public function getName()
+//     {
+//         return $this->name;
+//     }
+// }
+
+// $animal=new Animal('阿明'); //實例化 instant
+
+// echo $animal->name;
+// echo '顯示名稱:'.$animal->getName();
+// echo "<br>";
+
+// $animal->setName('小花');
+// echo '顯示名稱:'.$animal->name;
+// echo "<br>";
+
+
+// class Dog extends Animal
+// {
+//     function sit()
+//     {
+//         echo $this->name;
+//         echo "坐下";
+//     }
+// }
+
+
+
